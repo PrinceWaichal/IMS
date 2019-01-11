51c0abef120f3ca60cea2aecb1d1ea9d8f939ba0
Prince Waichal <princeironman@gmail.com>
2019-01-11 12:57:11 +0530

    Update 3.5

---
+++ IMS Package/1 - Software Package/FormPages/2 - SignUp/SignUpStruct.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace _1___Software_Package.FormPages._2___SignUp
+{
+    partial class SignUpStruct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ButtonPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.PrimeDButton = new System.Windows.Forms.Button();
+            this.Button = new System.Windows.Forms.Button();
+            this.EduDButton = new System.Windows.Forms.Button();
+            this.MiscDButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.ButtonPanel.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(308, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Management System";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(863, 26);
+            this.panel1.TabIndex = 6;
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.linkLabel2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.linkLabel2.Location = new System.Drawing.Point(434, 0);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(426, 31);
+            this.linkLabel2.TabIndex = 0;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Help And Feedback";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.linkLabel1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.linkLabel1.Location = new System.Drawing.Point(3, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(425, 31);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "About Us";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.linkLabel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.linkLabel2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 598);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(863, 31);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // ButtonPanel
+            // 
+            this.ButtonPanel.ColumnCount = 1;
+            this.ButtonPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.ButtonPanel.Controls.Add(this.PrimeDButton, 0, 0);
+            this.ButtonPanel.Controls.Add(this.EduDButton, 0, 1);
+            this.ButtonPanel.Controls.Add(this.MiscDButton, 0, 2);
+            this.ButtonPanel.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.ButtonPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.ButtonPanel.Location = new System.Drawing.Point(0, 26);
+            this.ButtonPanel.Name = "ButtonPanel";
+            this.ButtonPanel.RowCount = 6;
+            this.ButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.ButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.ButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.ButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.ButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.ButtonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.ButtonPanel.Size = new System.Drawing.Size(200, 572);
+            this.ButtonPanel.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.Button);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(200, 26);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(663, 572);
+            this.panel2.TabIndex = 8;
+            // 
+            // PrimeDButton
+            // 
+            this.PrimeDButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PrimeDButton.Location = new System.Drawing.Point(3, 3);
+            this.PrimeDButton.Name = "PrimeDButton";
+            this.PrimeDButton.Size = new System.Drawing.Size(194, 89);
+            this.PrimeDButton.TabIndex = 0;
+            this.PrimeDButton.Text = "Primary Details";
+            this.PrimeDButton.UseVisualStyleBackColor = true;
+            // 
+            // Button
+            // 
+            this.Button.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Button.Location = new System.Drawing.Point(0, 549);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(663, 23);
+            this.Button.TabIndex = 0;
+            this.Button.Text = "Enter Text";
+            this.Button.UseVisualStyleBackColor = true;
+            // 
+            // EduDButton
+            // 
+            this.EduDButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.EduDButton.Location = new System.Drawing.Point(3, 98);
+            this.EduDButton.Name = "EduDButton";
+            this.EduDButton.Size = new System.Drawing.Size(194, 89);
+            this.EduDButton.TabIndex = 1;
+            this.EduDButton.Text = "Educational Details";
+            this.EduDButton.UseVisualStyleBackColor = true;
+            // 
+            // MiscDButton
+            // 
+            this.MiscDButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MiscDButton.Location = new System.Drawing.Point(3, 193);
+            this.MiscDButton.Name = "MiscDButton";
+            this.MiscDButton.Size = new System.Drawing.Size(194, 89);
+            this.MiscDButton.TabIndex = 2;
+            this.MiscDButton.Text = "Miscellaneous Details";
+            this.MiscDButton.UseVisualStyleBackColor = true;
+            // 
+            // SignUpStruct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 629);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.ButtonPanel);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "SignUpStruct";
+            this.Text = "SignUpStruct";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ButtonPanel.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel ButtonPanel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button PrimeDButton;
+        private System.Windows.Forms.Button EduDButton;
+        private System.Windows.Forms.Button MiscDButton;
+        private System.Windows.Forms.Button Button;
+    }
+}
