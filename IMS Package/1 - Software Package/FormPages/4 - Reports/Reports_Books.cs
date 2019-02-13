0bff5c7db3088596b67cda7ba30464223f11b08e
Rutuja Sindkar <rutujasindkar0707@gmail.com>
2019-02-13 11:47:34 +0530

    Add "Reports";

---
+++ IMS Package/1 - Software Package/FormPages/4 - Reports/Reports_Books.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace _1___Software_Package.FormPages._3___DataEntry
+{
+    public partial class DataEntry_Reports : Form
+    {
+        public DataEntry_Reports()
+        {
+            InitializeComponent();
+        }
+    }
+}
