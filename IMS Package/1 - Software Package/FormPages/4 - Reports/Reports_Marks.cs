31a0bdb4d15f7ca1e95969831b9028b6a9c10cad
Prince Waichal <princeironman@gmail.com>
2019-02-15 07:20:20 +0530

    Add "Reports_PagesX2";

---
+++ IMS Package/1 - Software Package/FormPages/4 - Reports/Reports_Marks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1___Software_Package.FormPages._4___Reports
+{
+    public partial class Reports_Marks : Form
+    {
+        public Reports_Marks()
+        {
+            InitializeComponent();
+        }
+    }
+}
