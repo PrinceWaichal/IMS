082b36df43ddf912b50fecc498afbb7e437cad17
GUEST 1 <GUEST 1@DESKTOP-NNQ6IIH>
2019-02-20 15:00:28 +0530

    DataEntry validate

--- IMS Package/1 - Software Package/FormPages/3 - DataEntry/DataEntry_Exams.cs
+++ IMS Package/1 - Software Package/FormPages/3 - DataEntry/DataEntry_Exams.cs
@@ -71,6 +71,30 @@ namespace _1___Software_Package.FormPages._3___DataEntry
         private void Submit_Click(object sender, System.EventArgs e)
         {
             //Doing validation
+            if (ComboxSemester.Text == "")
+            {
+                MessageBox.Show("You have not entered option for semester", "Missing semester", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+            else if (ComboxCourse.Text == "")
+            {
+                MessageBox.Show("You have not entered option for course", "Missing course", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+            else if (ComboxSubject.Text == "")
+            {
+                MessageBox.Show("You have not entered option for Subject", "Missing Subject", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+            else if (comboxMonth.Text == "")
+            {
+                MessageBox.Show("You have not entered option for Month", "Missing Month", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+            else if (radioButRegular.Checked == false && radioButRepeater.Checked == true)
+            {
+                MessageBox.Show("You have not entered status corrrectly","Missing status of your exam",MessageBoxButtons.RetryCancel,MessageBoxIcon.Error);
+            }
+            else
+            {
+               
+            }
         }
     }
 }
