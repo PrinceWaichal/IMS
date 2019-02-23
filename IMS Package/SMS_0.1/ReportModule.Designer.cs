10690ec5ce040188b85065d469cefc4509ba0dff
Prince Waichal <princeironman@gmail.com>
2019-02-23 11:11:23 +0530

    Add "Report"

---
+++ IMS Package/SMS_0.1/ReportModule.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace SMS_0._1
+{
+    partial class ReportModule
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.CrystalReport1 = new SMS_0._1.CrystalReport();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = 0;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 27);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ReportSource = this.CrystalReport1;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(896, 408);
+            this.crystalReportViewer1.TabIndex = 1;
+            // 
+            // ReportModule
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.ClientSize = new System.Drawing.Size(896, 435);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "ReportModule";
+            this.Controls.SetChildIndex(this.crystalReportViewer1, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private CrystalReport CrystalReport1;
+    }
+}
---
+++ IMS Package/SMS_0.1/ReportModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SMS_0._1
+{
+    public partial class ReportModule : SMS_0._1.BaseFormApplication
+    {
+        public ReportModule()
+        {
+            InitializeComponent();
+        }
+    }
+}
